aa1db546cf4ab4e270648f87be319d48ae1e9d9c
protimsaha <protimsaha750@gmail.com>
2022-03-26 12:09:11 +0600

    main part done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -12,39 +12,49 @@ const Shop = () => {
     }, [])
 
     // set laptop on cart
-    const [cart, setCart] = useState([])
+    let [cart, setCart] = useState([])
 
     const addToCart = (laptop) => {
         const newCart = [...cart, laptop]
         setCart(newCart)
     }
 
+    const [choose, setChoose] = useState([])
+
     let chooseOne = () => {
         let choseItem = cart[Math.floor(Math.random() * cart.length)];
-        console.log(choseItem)
+        cart = []
+        setChoose(choseItem.name)
+        setCart(cart)
     }
-
-    if (cart.length <= 4) {
-        // let choseItem = cart[Math.floor(Math.random() * cart.length)];
+    let chooseAgain = () => {
+        setChoose([])
+        setCart([])
     }
 
     return (
         <div className='shop-container'>
+            {/* <div className='row'> */}
             <div className="product">
                 {
                     laptops.map(laptop => <Laptop laptop={laptop}
                         addToCart={addToCart} key={laptop.id}></Laptop>)
                 }
             </div>
-            <div className="cart">
+            <div className="cart ">
                 {
                     cart.map(item => <h2 key={item.id}>{item.name}</h2>)
                 }
+                <div className='choosen'>
+                    <h3>Chosen Item</h3>
+                    <h1>{choose}</h1>
+                </div>
                 <div>
                     <button onClick={chooseOne} className="btn-group">Choose 1 for me</button>
-                    <button className="btn-group">Choose again</button>
+                    <button onClick={chooseAgain} className="btn-group">Choose again</button>
                 </div>
             </div>
+            {/* </div> */}
         </div>
 
     );
